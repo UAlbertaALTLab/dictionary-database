bab463ee68781ce3af2bf870db0b84e41e0e7dbf
Daniel W. Hieber <dwhieb@gmail.com>
2021-04-04 14:41:08 -0500

    MD test suite

--- test/getTestEntry.js
+++ test/getTestEntry.js
@@ -2,11 +2,12 @@ import { expect } from 'chai';
 
 /**
  * Finds the first entry in the test database whose \test field matches the test description
- * @param  {Object} ctx The Mocha test context
- * @return {Object}     Returns the relevant test entry
+ * @param  {Array}  data      The array of test database entries (within a Mocha test: this.data).
+ * @param  {String} testTitle The title of the test (within a Mocha test: this.test.title).
+ * @return {Object}           Returns the relevant test entry
  */
-export default function getTestEntry(ctx) {
-  const matchedEntry = ctx.data.find(entry => entry.test === ctx.test.title);
-  if (!matchedEntry) expect.fail(`No test database entry found for test "${ctx.test.title}"`);
+export default function getTestEntry(data, testTitle) {
+  const matchedEntry = data.find(entry => entry.test === testTitle);
+  if (!matchedEntry) expect.fail(`No test database entry found for test "${testTitle}"`);
   return matchedEntry;
 }
